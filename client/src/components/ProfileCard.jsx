f0eb5f1f4b47ef3271ad6c6017e7f492bc4af123
dhananjayyadav21 <dhananjayyadav1807@gmail.com>
2025-04-18 00:51:06 +0530

    fix profile component style

--- client/src/components/ProfileCard.jsx
+++ client/src/components/ProfileCard.jsx
@@ -4,34 +4,36 @@ const ProfileCardWithBanner = () => {
     const user = {
         name: "Jane Smith",
         email: "jane.smith@example.com",
-        phone: "+91-9876543210",
-        avatar: "https://i.pravatar.cc/150?img=15",
-        about: "React developer passionate about building modern web applications."
+        isVerified: "+91-9876543210",
+        Profile: "",
+        About: ""
       };
 
       
   return (
-    <div className="container my-5">
-      <div className="row bg-white rounded-4 shadow overflow-hidden">
+    <div className="container p-4">
+      <div className="row bg-white rounded-3 shadow overflow-hidden">
         
-        {/* User Details */}
-        <div className="col-12 col-md-6 p-4 d-flex flex-column justify-content-center">
-          <div className="d-flex align-items-center mb-4">
-            <img
-              src={user.avatar || "https://via.placeholder.com/150"}
-              alt="User Avatar"
-              className="rounded-circle me-3 userprofile-img"
-            />
-            <div>
-              <h4 className="mb-1">{user.name}</h4>
-              <p className="mb-0 text-muted">{user.email}</p>
-              <small className="text-muted">{user.phone}</small>
-            </div>
+        {/* User Details d-flex align-items-center d-flex flex-column justify-content-center */}
+        <div className="col-12 col-md-6 p-4 ">
+          <div className="row d-flex align-items-center">
+            <div className="col-12 col-md-3 mb-4 d-flex justify-content-center"> 
+              <img
+                src={user.Profile || "/assets/img/Avtar.jpg"}
+                alt="User Avatar"
+                className="rounded-circle me-3 userprofile-img"
+              />
+              </div>
+              <div className="col-12 col-md-9 mb-4">
+                <h4 className="mb-1">{user.name}</h4>
+                <p className="mb-0 text-muted">{user.email}</p>
+                <small className="text-muted">{user.isVerified}</small>
+              </div>
           </div>
 
           <div>
             <h6 className="fw-semibold">About</h6>
-            <p className="text-muted mb-0">{user.about || "No bio provided."}</p>
+            <p className="text-muted mb-0">{user.About || "One lesson at a time, one step closer to greatness."}</p>
           </div>
         </div>
 
@@ -43,6 +45,10 @@ const ProfileCardWithBanner = () => {
             className="img-fluid h-100 w-100 banner-image"
           />
         </div>
+      </div>
+
+      <div className="d-flex justify-content-center my-5">
+        <img className="d-flex justify-content-center" src="/assets/img/brandlog.png" alt="Examee" style={{width:"210px"}} />
       </div>
     </div>
   );
