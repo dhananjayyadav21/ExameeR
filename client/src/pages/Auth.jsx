fba6870a044adc60bee2fa2a1cdcae875e660b6e
dhananjayyadav21 <dhananjayyadav1807@gmail.com>
2025-04-18 00:10:57 +0530

    fix auth button style

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -4,34 +4,34 @@ import Register from '../components/Register'
 
 const Auth = () => {
 
-    const [LoginDisplay, setLoginDisplay] = useState("none");
-    const [RegisterDisplay, setRegisterDisplay] = useState("");
+    const [LoginDisplay, setLoginDisplay] = useState({D:"none", TextColor:"#000000", BackgroundColorColor:"#ffffff" });
+    const [RegisterDisplay, setRegisterDisplay] = useState({D:"", TextColor:"#04bd20", BackgroundColorColor:"#f8f9fa" });
 
     const openLogin = ()=>{
-        setLoginDisplay("");
-        setRegisterDisplay("none");
+        setLoginDisplay({D:"", TextColor:"#04bd20", BackgroundColorColor:"#f8f9fa" });
+        setRegisterDisplay({D:"none", TextColor:"#000000", BackgroundColorColor:"#ffffff" });
     }
 
     const openRegister = ()=>{
-        setRegisterDisplay("");
-        setLoginDisplay("none");
+        setRegisterDisplay({D:"", TextColor:"#04bd20", BackgroundColorColor:"#f8f9fa" });
+        setLoginDisplay({D:"none", TextColor:"#000000", BackgroundColorColor:"#ffffff" });
     }
 
   return (
     <> 
         <div className="container">
         <div className="Auth-header shadow-sm col-12 col-lg-5 container d-flex justify-content-center align-self-center gap-3 py-2 px-3 mt-5 rounded-3">
-            <button className="col-6 btn rounded-2" onClick={openRegister}>REGISTER</button>
-            <button className='col-6 btn rounded-2' onClick={openLogin} >LOGIN</button>
+          <button className="col-6 btn rounded-2" style={{ color:RegisterDisplay.TextColor, backgroundColor:RegisterDisplay.BackgroundColorColor}} onClick={openRegister}>REGISTER</button>
+          <button className="col-6 btn rounded-2" style={{ color:LoginDisplay.TextColor, backgroundColor:LoginDisplay.BackgroundColorColor}} onClick={openLogin} >LOGIN</button>
         </div>
         </div>
         
-        <div className={`d-${LoginDisplay}`}> 
+        <div className={`d-${LoginDisplay.D}`}> 
         <Login/>
         </div>
            
-        <div className={`d-${RegisterDisplay}`}> 
-          <Register/>
+        <div className={`d-${RegisterDisplay.D}`}> 
+          <Register />
         </div>
     </>
   )
