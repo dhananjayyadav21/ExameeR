f35086ca849614f0424f13a6d1ca155ee25823be
dhananjayyadav21 <dhananjayyadav1807@gmail.com>
2025-04-20 22:05:46 +0530

    create all public notes from db

--- Backend/controllers/ContentController.js
+++ Backend/controllers/ContentController.js
@@ -16,6 +16,7 @@ const uploadNotes = async (req, res) => {
       })
     }
 
+    // check user role 
     if (user.Role !== "Admin" && user.Role !== "Instructor") {
       return res.status(401).json({
         success: false,
@@ -33,7 +34,7 @@ const uploadNotes = async (req, res) => {
       });
     }
 
-    const newNote = new Note({
+    const newNote = new Note({ //create notes
       title,
       description,
       professor,
@@ -48,20 +49,20 @@ const uploadNotes = async (req, res) => {
       deletedOn: null
     });
 
-    await newNote.save();
+    await newNote.save(); // save notes in db
     let note = {
       title,
       professor,
       category
     }
 
-    return res.status(201).json({
+    return res.status(201).json({ // send result as true
       success: true,
       message: 'Note uploaded successfully',
       note
     })
 
-  } catch (error) {
+  } catch (error) { // if accuerd error
     console.error('Upload Note Error:', error);
     return res.status(201).json({
       success: false,
@@ -70,4 +71,46 @@ const uploadNotes = async (req, res) => {
   }
 };
 
-module.exports = { uploadNotes }
+// ------[Get all public notes ] -----------------
+const getAllPublicNotes = async (req, res) => {
+
+  try {
+    let userId = req.user._id;
+
+    // check user exist or not
+    const user = await userModel.findById(userId).select('-Password -ForgotPasswordCode');
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: 'User not found !',
+      })
+    }
+
+    if (user.isVerified !== true) { // check user verified  or not
+      return res.status(401).json({
+        success: false,
+        message: "You are not verified user!",
+      })
+    }
+
+    const notes = await Note.find({ // all notes find from db
+      isPublic: true,
+      status: 'public'
+    }).sort({ createdOn: -1 });
+
+    res.status(200).json({ // return result as true
+      success: true,
+      count: notes.length,
+      data: notes,
+    });
+  } catch (error) { // if accured some error
+    console.error('Error fetching public notes:', error);
+    res.status(500).json({
+      success: false,
+      message: 'Server Error while fetching public notes',
+    });
+  }
+};
+
+
+module.exports = { uploadNotes, getAllPublicNotes }
--- Backend/routes/Content.js
+++ Backend/routes/Content.js
@@ -5,6 +5,8 @@ const router = require('express').Router();
   //--- Content uplodes ---------
 
   router.post('/addNotes',fetchUser, ContentController.uploadNotes);
+
+  router.get('/getAllPublicNotes',fetchUser, ContentController.getAllPublicNotes);
   
 
 module.exports = router;  
