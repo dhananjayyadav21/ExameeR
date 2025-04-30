6558763023b3decf182ae9e746f10f7deca648f3
dhananjayyadav21 <dhananjayyadav1807@gmail.com>
2025-04-30 19:38:35 +0530

    add profile in team and fix offers premiam  card title

--- client/src/components/Team.jsx
+++ client/src/components/Team.jsx
@@ -3,14 +3,13 @@ import React from 'react'
 const Team = (props) => {
 
     const {name,role,description,profile} = props
-    console.log(profile)
 
   return (
     <>
         <div className='col-12 col-sm-6 col-md-4'>
             <div className='card rounded-4 shadow-sm p-3 card-transition team h-100'>
                 <div className="d-flex gap-2">
-                    <img src="https://wallpapers.com/images/hd/professional-profile-pictures-1350-x-1080-sizz773bu8k11plw.jpg" alt="Professor" className="rounded-circle" loading="lazy" style={{width:"60px", height:"60px"}} />
+                    <img src={profile || "/assets/img/User.jpg" }alt="Professor" className="rounded-circle" loading="lazy" style={{width:"60px", height:"60px"}} />
                     <div>
                     <h5 className="m-0 p-0">{name}</h5>
                     <p className="my-2 text-secondary">{description}</p>
