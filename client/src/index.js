1f10751d00ec00d66839e7206cbe32149d68f888
Dhananjay Yadav <dhananjayyadav1807@gmail.com>
2025-05-03 14:40:08 +0530

    add static file

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,25 @@ import App from './App';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Determine the container to render into
+const path = window.location.pathname;
+const targetId = path === '/' ? 'main' : 'root'; // Render into <main> only on "/"
+let container = document.getElementById(targetId);
+
+if (targetId === 'main') {
+  container.classList.remove('d-none');
+}
+
+// Safety check in case the element doesn't exist
+if (!container) {
+  console.error(`Element with id="${targetId}" not found in index.html`);
+  container = document.getElementById("root");
+}
+
+const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
     <ToastContainer />
   </React.StrictMode>
 );
-
