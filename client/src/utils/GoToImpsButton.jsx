978f839d4231b7717622b6256422618b9e8d32fc
Dhananjay Yadav <dhananjayyadav1807@gmail.com>
2025-08-02 00:56:15 +0530

    Add imps page

--- client/src/utils/GoToImpsButton.jsx
+++ client/src/utils/GoToImpsButton.jsx
@@ -1,37 +1,16 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React from 'react';
 import { FaArrowUp } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
 const GoToImpsButton = () => {
     const navigate = useNavigate();
-    const [isFixed, setIsFixed] = useState(false);
-    const buttonRef = useRef(null);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            const scrollY = window.scrollY;
-
-            if (scrollY > 300 && !isFixed) {
-                setIsFixed(true);
-            } else if (scrollY <= 300 && isFixed) {
-                setIsFixed(false);
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, [isFixed]);
 
     const handleClick = () => {
-        navigate('/imps');
+        navigate('/ImpsPage');
     };
 
     return (
-        <div
-            ref={buttonRef}
-            className={`go-to-imps ${isFixed ? 'fixed' : 'absolute'}`}
-            onClick={handleClick}
-            title="Go to Important Page"
+        <div className="go-to-imps" onClick={handleClick}
         >
             <FaArrowUp className="arrow-icon" />
         </div>
